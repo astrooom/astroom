f0826fe07c3cf2d9599b11ea87f42016743bdb31
Olle Ljung <olle.ljung@gmail.com>
2024-02-26 23:16:22 +0100

    Removed unused navbar links

--- utils/navigation.ts
+++ utils/navigation.ts
@@ -18,14 +18,14 @@ export const navigation: Navigation = {
       name: "About",
       href: "/about",
     },
-    {
-      name: "Projects",
-      href: "/projects",
-    },
-    {
-      name: "Contact",
-      href: "/contact",
-    },
+    // {
+    //   name: "Projects",
+    //   href: "/projects",
+    // },
+    // {
+    //   name: "Contact",
+    //   href: "/contact",
+    // },
   ],
 }
 
