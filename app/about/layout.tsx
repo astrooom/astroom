6dab9ddae37aae3bce46bff0ebc27430547b9df7
Olle Ljung <olle.ljung@gmail.com>
2024-02-28 11:55:44 +0100

    Reverted to sm: tag

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -5,7 +5,7 @@ export default function AboutLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="mt-24 lg:max-w-[76rem] max-w-[22rem] mx-auto">
+    <div className="mt-24 sm:max-w-[76rem] max-w-[22rem] mx-auto">
       {children}
     </div>
   );
